91146d68ef6d55019dc0e806306fcd0a49df2a69
Vishruta_Patil <vishrutapatil2106@gmail.com>
2022-09-28 13:00:48 +0530

    feat: cors configuration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const connectDB = require("./db/db.connect")
 const pageNotFound = require("./middleware/pageNotFound")
+const cors = require("cors")
 
 const product = require("./routes/product.routes")
 const category = require('./routes/category.routes')
@@ -8,8 +9,15 @@ const auth = require('./routes/auth.routes')
 const cart = require('./routes/cart.routes')
 const wishlist = require('./routes/wishlist.routes')
 
+const corsOptions = {
+    origin: process.env.PORT,
+    optionsSuccessStatus: 200
+}
+
 const app = express()
 app.use(express.json())
+app.use(cors(corsOptions))
+
 connectDB()
 
 const port = process.env.PORT || 8000
